6bb0f7550f9b15494ad37ff9bc7815406239c730
Vansh Patel <vasupatel893@gmail.com>
2026-09-08 10:17:43 +0530

    Delete shape directory

--- shape/cir.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def area(r):
-    return math.pi * r * r
-
-def circumference(r):
-    return 2 * math.pi * r
--- shape/rec.py
+++
@@ -1,5 +0,0 @@
-def area(l, w):
-    return l * w
-
-def perimeter(l, w):
-    return 2 * (l + w)
